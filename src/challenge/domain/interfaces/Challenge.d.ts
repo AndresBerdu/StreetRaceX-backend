14b2248f4302ae0f54602c98c5eeb5fc5463c7aa
MELANNY CORDOBA <101124668+melanny0822@users.noreply.github.com>
2026-05-08 10:07:25 -0500

    feature enums tipoCarrera

--- src/challenge/domain/interfaces/Challenge.d.ts
+++ src/challenge/domain/interfaces/Challenge.d.ts
@@ -6,6 +6,22 @@ export type ChallengeStatus =
   | "STARTED"
   | "COMPLETED";
 
+export enum TipoCarrera {
+  CUARTO_MILLA = 'CUARTO_MILLA',
+  VUELTAS = 'VUELTAS',
+  DERRAPE = 'DERRAPE',
+}
+
+export type Type_race = "quarter_mile" | "loops" | "drif";
+
+export type State =
+  | "pending"
+  | "accepted"
+  | "rejected"
+  | "in_progress"
+  | "completed"
+  | "cancel";
+
 export interface Challenge {
   id?: string;
   title: string;
